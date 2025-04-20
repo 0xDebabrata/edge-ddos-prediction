58f7712b9f6f8462c758d630e56ee02a7ffe31a2
Debabrata Mondal <debabrata.js@protonmail.com>
2025-04-20 11:32:06 +0530

    improve code

--- sdn/network-topology.py
+++ sdn/network-topology.py
@@ -19,14 +19,15 @@ def customNet():
     )
 
     info("Adding hosts\n")
-    h1, h2, h3, h4, h5, h6, h7, h8, h9, h10 = [net.addHost(h) for h in ("h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8", "h9", "h10")]
+    n_hosts = 10
+    hosts  = [net.addHost(f"h{idx}") for idx in range(n_hosts)]
 
     info("Adding switches\n")
     s1 = net.addSwitch("s1")
     # s2 = net.addSwitch("s2")
 
     info("Adding switch links\n")
-    for h in [h1, h2, h3, h4, h5, h6, h7, h8, h9, h10]:
+    for h in hosts:
         net.addLink(h, s1)
 
     # for h in [h6, h7, h8, h9, h10]:
